e8d58436ffadab63856c4851a3cd5b1b53a2e3fa
ThabisoBBD <Thabiso.Bhembe@bbd.co.za>
2025-02-03 20:26:56 +0200

    donee

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function getWeather(position) {
    const long = position.coords.longitude;
 
    const APIkey = 'dc09c9239910d461bdd17cc4c1d2b9d2';
-//    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${}&lon=${long}&appid=${APIkey}`;
    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${Lat}&lon=${long}&appid=${APIkey}`
 
    fetch(url)
@@ -17,8 +16,10 @@ function getWeather(position) {
     .then(data=> {
         
         document.getElementById('weather').innerHTML= `
-        <h2>${data.name} - ${data.sys.country}</h2>
+        <h1>${data.sys.country} - ${data.name}</h1>
+        <p> ${data.weather[0].main}</p>
         <p> ${data.weather[0].description}</p>
+       
         `
     })
    
